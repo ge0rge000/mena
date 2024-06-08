665ef3c2a86a2a2ef8367f79c3c4313bc813febe
george samy <gogosony124@gmail.com>
2024-06-08 16:44:43 +0400

    asdsadsa

--- app/Http/Controllers/Api/Slider/SliderController.php
+++ app/Http/Controllers/Api/Slider/SliderController.php
@@ -1,19 +1,26 @@
 <?php
-
 namespace App\Http\Controllers\Api\Slider;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Slider;
+
 class SliderController extends Controller
 {
-    public function slider($year_type){
+    public function slider($year_type)
+    {
+        $base_url = 'https://menamaherbigben.com/storage/photos/';
+        $sliders = Slider::where("year_type", $year_type)->get(['title', 'image']);
 
-        $sliders=Slider::where("year_type",$year_type)->get();
-        return response(
-            [ 'message'=>"sliders are get",
-             'status'=> true,
-             'data'=> $sliders]
-        ,200);
+        $data = $sliders->map(function ($slider) use ($base_url) {
+            return [
+                'title' => $slider->title,
+                'image' => $base_url . $slider->image,
+            ];
+        });
+
+        return response([
+            $data
+        ], 200);
     }
 }
