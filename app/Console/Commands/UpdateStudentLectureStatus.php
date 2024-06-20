7aa8b93a13852451ba77a23738f77ac1d49c16cc
george samy <gogosony124@gmail.com>
2024-06-20 16:16:12 +0300

    asdsad

---
+++ app/Console/Commands/UpdateStudentLectureStatus.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use DB;
+
+class UpdateStudentLectureStatus extends Command
+{
+    protected $signature = 'update:student-lecture-status';
+    protected $description = 'Update status column in student_lecture table every month';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        DB::table('student_lecture')->update(['status' => 0]);
+        $this->info('Student lecture status updated successfully.');
+    }
+}
