6c196cc327627fa069dcf0eb485b9eabefb751e4
george samy <gogosony124@gmail.com>
2024-06-20 20:19:44 +0300

    asdsad

--- app/Http/Controllers/Api/Students/StudentController.php
+++ app/Http/Controllers/Api/Students/StudentController.php
@@ -9,20 +9,21 @@ use DB;
 class StudentController extends Controller
 {
     public function checkLectures($id)
-{
-    $student = User::find($id);
-    if (!$student) {
-        return response()->json(['error' => 'Student not found'], 404);
+    {
+        $student = User::find($id);
+        if (!$student) {
+            return response()->json(['error' => 'Student not found'], 404);
+        }
+
+        $lectureIds = DB::table('lectures')
+                        ->join('student_lecture', 'lectures.id', '=', 'student_lecture.lecture_id')
+                        ->where('student_lecture.user_id', '=', $id)
+                        ->where('lectures.status', '=', 1)  // Specify the table name for the status column
+                        ->pluck('lectures.id');
+
+        return response()->json($lectureIds);
     }
 
-    $lectureIds = DB::table('lectures')
-                    ->join('student_lecture', 'lectures.id', '=', 'student_lecture.lecture_id')
-                    ->where('student_lecture.user_id', '=', $id)
-                    ->where('status', '=', 1)
-                    ->pluck('lectures.id');
-
-    return response()->json($lectureIds);
-}
 
  public function checkUnits($id)
 {
