0fa8f8a0ac5753d81f7806dce11560a0486912a5
george samy <gogosony124@gmail.com>
2024-06-20 19:21:26 +0300

    dasd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:student-lecture-status')->everyMinute();
+        $schedule->command('update:student-lecture-status')->everyTwoMinutes();
     }
 
     /**
