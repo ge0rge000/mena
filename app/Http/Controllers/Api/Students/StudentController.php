aec05762e1eab95b7ff8ae49b5eb3cdc6438fbc7
george samy <gogosony124@gmail.com>
2024-06-20 19:53:25 +0300

    asdsad

--- app/Http/Controllers/Api/Students/StudentController.php
+++ app/Http/Controllers/Api/Students/StudentController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\Students;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use DB;
 class StudentController extends Controller
 {
     public function checkLectures($id)
@@ -22,17 +22,22 @@ class StudentController extends Controller
 
         return response()->json($lectureIds);
     }
-    public function checkUnits($id)
-    {
-        $student = User::find($id);
-        if (!$student) {
-            return response()->json(['error' => 'Student not found'], 404);
-        }
-
-        $unitIds = $student->units()->where('status', '=', 1)->pluck('id');
-
-        return response()->json($unitIds);
+ public function checkUnits($id)
+{
+    $student = User::find($id);
+    if (!$student) {
+        return response()->json(['error' => 'Student not found'], 404);
     }
+
+    $unitIds = DB::table('units')
+                 ->join('student_unit', 'units.id', '=', 'student_unit.unit_id')
+                 ->where('student_unit.user_id', '=', $id)
+                 ->where('status', '=', 1)
+                 ->pluck('units.id');
+
+    return response()->json($unitIds);
+}
+
 
     public function returnwallet($id)
     {
